3754af2eb78bbf0b3d33336660911702ea2bf9d6
Dmitry Barsukov <rizzzhik@gmail.com>
2022-06-30 23:31:36 +0300

    [ADD] GitHub CI

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="optimize_tensorrt",
-    version="1.1",
+    version="1.0",
     description="Convert and inference TensorRT models",
     license="Apache",
     author="Dmtry Barsukov",
