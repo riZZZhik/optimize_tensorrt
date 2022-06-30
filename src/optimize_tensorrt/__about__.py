d59a6810bc9de7609bc7043d0da3329a2b71f95b
Dmitry Barsukov <rizzzhik@gmail.com>
2022-06-30 23:15:13 +0300

    [ADD] About

---
+++ src/optimize_tensorrt/__about__.py
@@ -0,0 +1,8 @@
+__title__ = "Optimize-TensorRT"
+__description__ = "Optimize and Inference TensorRT Machine Learning models in Python"
+__url__ = "https://github.com/riZZZhik/optimize_tensorrt"
+__version_info__ = ("1", "0", "0")
+__version__ = ".".join(__version_info__)
+__author__ = "Dmitry Barsukov"
+__author_email__ = "rizzzhik@gmail.com"
+__license__ = "Apache"
