e884a5d8333043e717a28926c94e27b112462656
Dmitry Barsukov <rizzzhik@gmail.com>
2022-06-30 23:39:37 +0300

    [FIX] Version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="optimize_tensorrt",
-    version="1.0",
+    version="1.0.1",
     description="Convert and inference TensorRT models",
     license="Apache",
     author="Dmtry Barsukov",
