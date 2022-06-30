cd0cfd0102374254dca78d3e7721cc9dcfa220b4
Dmitry Barsukov <rizzzhik@gmail.com>
2022-06-30 23:19:41 +0300

    [ADD] Setup for PyPI

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="optimize_tensorrt",
+    version="1.1",
+    description="Convert and inference TensorRT models",
+    license="Apache",
+    author="Dmtry Barsukov",
+    author_email="riZZZhik@gmail.com",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    url="https://github.com/gmyrianthous/example-publish-pypi",
+    keywords="https://github.com/riZZZhik/optimize_tensorrt",
+    install_requires=["numpy", "loguru"],
+)
